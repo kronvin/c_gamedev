bfcb5001c784a1ec9ceaff69082e6be269918d39
kronvin <kronvin@gmail.com>
2025-01-18 21:38:34 +0200

    Adding 9th homework

---
+++ HW_9/Functions.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//1
+void TranslateArray(int numbers[], int size);
+
+//2
+void ToUppercase(char str[]);
+
+//3
+bool IsPalindrome(const char str[]);
+
+//4
+static bool IsVowel(char n);
+void CountVowCons(const char string[], int& vowelscount, int& consoncount);
+
+//5
+bool IsEqual(const char str1[], const char str2[]);
